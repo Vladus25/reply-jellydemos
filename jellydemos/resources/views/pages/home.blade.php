27c6114de85025667233acf52709bc03f3dcf4de
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-08-09 19:57:17 +0200

    About completed

--- jellydemos/resources/views/pages/home.blade.php
+++ jellydemos/resources/views/pages/home.blade.php
@@ -37,8 +37,59 @@
         </section>
 
         {{-- ABOUT SECTION --}}
-        <section>
-            <h1>DFSDFSDF</h1>
+        <section class="about">
+
+            <div class="about-us">
+                <h2>
+                    About Us
+                </h2>
+            </div>
+
+            <div class="subtitle">
+                <p>We love restaurants as much as you do. That’s why we’ve been helping them fill tables since 1999. Welcome to elixir restaurant</p>
+            </div>
+
+            <div class="about-container-1">
+
+                <div class="text-container">
+                    <h2>The History</h2>
+                    <p class="text">The <strong>History of Kitchens</strong>  and Cooks gives further intimation on Mr Boulanger usual menu, stating confidently that "Boulanger served salted poultry and fresh eggs, all presented without a tablecloth on small marble tables". Numerous commentators have also referred to the supposed restaurant owner's eccentric habit of touting for custom outside his establishment, dressed in aristocratic fashion and brandishing a sword</p>
+                    <p class="text">According to Miss Spang, there is not a shred of evidence for any of it. She said: These legends just get passed on by hearsay and then spiral out of control. Her interest in <strong>Boulanger</strong>  dates back to a history of food seminar in Paris in the mid-1990s</p>
+                    <div class="ornament"></div>
+                </div>
+
+                <div class="img-container">
+                    <img src="{{ asset('storage/img/about-1.jpg') }}" alt="">
+                </div>
+            </div>
+
+            <div class="about-container-2">
+
+                <div class="img-container">
+                    <img src="{{ asset('storage/img/about-4.jpg') }}" alt="">
+
+                    <div class="owl-buttons">
+                        <div class="owl-prev"></div>
+                        <div class="owl-next"></div>
+                    </div>
+
+                    <div class="owl-pagination">
+                        <div class="owl-page active"></div>
+                        <div class="owl-page"></div>
+                        <div class="owl-page"></div>
+                    </div>
+                </div>
+
+                <div class="text-container">
+                    <p class="text">The <strong>History of Kitchens</strong>  and Cooks gives further intimation on Mr Boulanger usual menu, stating confidently that "Boulanger served salted poultry and fresh eggs, all presented without a tablecloth on small marble tables". Numerous commentators have also referred to the supposed restaurant owner's eccentric habit of touting for custom outside his establishment, dressed in aristocratic fashion and brandishing a sword</p>
+                    <p class="text">According to Miss Spang, there is not a shred of evidence for any of it. She said: These legends just get passed on by hearsay and then spiral out of control. Her interest in <strong>Boulanger</strong>  dates back to a history of food seminar in Paris in the mid-1990s</p>
+                </div>
+            </div>
+        </section>
+
+        {{-- TIMETABLE SECTION --}}
+        <section class="timetable">
+
         </section>
     </main>
 
